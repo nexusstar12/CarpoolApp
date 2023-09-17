4c993e2f8a76a898619a5636041a56a23060d7f4
nasalito9 <45346008+nasalito9@users.noreply.github.com>
2023-09-16 18:08:26 -0700

    Added team page, individual templates for team members

--- application/poolapp/src/main/java/com/fantasticfour/poolapp/HelloController.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/HelloController.java
@@ -1,14 +1,12 @@
 package com.fantasticfour.poolapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
-
-    @GetMapping("/")
+    @RequestMapping(value = "/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "aboutMe.html";
     }
-
 }
