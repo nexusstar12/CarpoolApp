f5f60210aef9b7f6a821e338aa38555cf234355f
nexusstar12 <Nexusstar12@gmail.com>
2023-10-06 13:00:57 -0700

    User has no errors when running the application, all properties of User (ID, first/last name, phone number, and email) working fine

--- application/poolapp/src/main/java/com/fantasticfour/poolapp/domain/Account.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/domain/Account.java
@@ -1,18 +1,11 @@
-package com.fantasticfour.poolapp.domain;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.JoinColumn;
-
-@Entity
-public class Account extends User {
-
-    private String username;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "password_id")
-    private Password password;
-
-    // Getters Setters
-}
+//package com.fantasticfour.poolapp.domain;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.OneToOne;
+//import jakarta.persistence.CascadeType;
+//import jakarta.persistence.JoinColumn;
+//
+//@Entity
+//public class Account extends User {
+//
+//}
--- application/poolapp/src/main/java/com/fantasticfour/poolapp/domain/Passenger.java
+++
@@ -1,5 +0,0 @@
-package com.fantasticfour.poolapp.domain;
-
-import jakarta.persistence.Entity;
-public class Passenger extends User{
-}
