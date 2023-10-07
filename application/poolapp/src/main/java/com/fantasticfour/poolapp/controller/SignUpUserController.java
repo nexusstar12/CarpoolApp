017f4d2324dd46890342ca01141f41a50d3458f6
kkrstchn <kristiangoranov@yahoo.com>
2023-10-07 15:48:12 -0700

    added account, password  entity functionality, with repositories, services, and add user

--- application/poolapp/src/main/java/com/fantasticfour/poolapp/controller/SignUpUserController.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/controller/SignUpUserController.java
@@ -1,7 +1,11 @@
 package com.fantasticfour.poolapp.controller;
 
+import com.fantasticfour.poolapp.domain.Account;
+import com.fantasticfour.poolapp.domain.Password;
 import com.fantasticfour.poolapp.domain.User;
-import com.fantasticfour.poolapp.services.UserServce;
+import com.fantasticfour.poolapp.services.AccountService;
+import com.fantasticfour.poolapp.services.PasswordService;
+import com.fantasticfour.poolapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,18 +14,68 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/signup")
 public class SignUpUserController {
 
     @Autowired
-    private UserServce userServce;
+    private UserService userService;
+
+    @Autowired
+    private PasswordService passwordService;
+
+    @Autowired
+    private AccountService accountService;
+
+//    @PostMapping("/")
+//    public ResponseEntity<User> addUser (@RequestBody User user) {
+//        User newUser = userServce.addUser(user);
+//
+//        //TODO: ADD VALIDATION USER IS ADDED
+//        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+//    }
 
     @PostMapping("/")
-    public ResponseEntity<User> addUser (@RequestBody User user) {
-        User newUser = userServce.addUser(user);
+    public ResponseEntity<Map<String, Object>> addUser (@RequestBody Map<String, String> jsonMap) {
+        jsonMap.forEach((key, value) -> System.out.println("Key: " + key + ", Value: " + value));
 
-        //TODO: ADD VALIDATION USER IS ADDED
-        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+        //create new user
+        String firstName = jsonMap.get("firstName");
+        String lastName = jsonMap.get("lastName");
+        String name = firstName + " " + lastName;
+
+        User newUser = new User();
+        newUser.setFirstName(firstName);
+        newUser.setLastName(lastName);
+        newUser.setEmail(jsonMap.get("email"));
+        newUser.setName(name);
+
+        //create new password
+        //TODO: HASH PASSWORD
+        Password newPassword = new Password();
+        newPassword.setPassword(jsonMap.get("password"));
+
+        //create new account that links user and password
+        Account newAccount = new Account();
+        newAccount.setUser(newUser);
+        newAccount.setPassword(newPassword);
+
+        //add users and password to DB table
+        //and add users and password to the account table
+       User addedUser = userService.addUser(newUser);
+       Password addPassword = passwordService.addPassword(newPassword);
+       Account addAccount = accountService.addAccount(newAccount);
+
+       //build custom json response body
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("user", addedUser);
+        responseMap.put("password", addPassword);
+        responseMap.put("account", addAccount);
+
+        //TODO: ADD VALIDATION USER, account, and paswword are ADDED
+        return new ResponseEntity<>(responseMap, HttpStatus.CREATED);
     }
 }
--- application/poolapp/src/main/java/com/fantasticfour/poolapp/services/UserService.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/services/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServce {
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
