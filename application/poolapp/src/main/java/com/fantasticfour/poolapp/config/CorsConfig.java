437afa8a3d3b79f1fa5e0d85d4c922db6ab44764
nexusstar12 <Nexusstar12@gmail.com>
2023-10-07 22:49:15 -0700

    Implemented a CorsConfig class to hopefully resolve CORS issue with FE and BE.

---
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.fantasticfour.poolapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // All endpoints in the API
+                .allowedOrigins("http://localhost:3000") // Your frontend app origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // Allowed request methods
+                .allowCredentials(true) // Credentials, cookies, HTTP authentication
+                .maxAge(3600) // Pre-flight request cache duration
+                .allowedHeaders("*"); // Allowed headers
+    }
+}
