14e107d2ee011edafb578fd4572a512b1bd3bce7
kkrstchn <kristiangoranov@yahoo.com>
2023-10-10 00:26:15 -0700

    added sign in api endpoint. no password hashing yet.

--- application/poolapp/src/main/java/com/fantasticfour/poolapp/controller/SignInController.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/controller/SignInController.java
@@ -1,31 +1,78 @@
 package com.fantasticfour.poolapp.controller;
 
+import com.fantasticfour.poolapp.domain.Account;
+import com.fantasticfour.poolapp.domain.Password;
+import com.fantasticfour.poolapp.domain.User;
+import com.fantasticfour.poolapp.repository.AccountRepository;
+import com.fantasticfour.poolapp.repository.PasswordRepository;
+import com.fantasticfour.poolapp.repository.UserRepository;
 import com.fantasticfour.poolapp.services.AccountService;
 import com.fantasticfour.poolapp.services.PasswordService;
 import com.fantasticfour.poolapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/signin")
 public class SignInController {
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @Autowired
-    private PasswordService passwordService;
+    private PasswordRepository passwordRepository;
 
     @Autowired
-    private AccountService accountService;
+    private AccountRepository accountRepository;
 
-//    @GetMapping("/")
-//    public ResponseEntity<Map<String, Object>> signInUser (@RequestBody Map<String, String> jsonMap) {
-//
-//    }
+    @GetMapping("/")
+    public ResponseEntity<List<Object>> signInUser (@RequestBody Map<String, String> jsonMap) {
+
+        jsonMap.forEach((key, value) -> System.out.println("Key: " + key + ", Value: " + value));
+        List<Object> responseList = new ArrayList<>();
+
+        //user to response list if email exists.
+       Optional<User> optionalUser = userRepository.findByEmail(jsonMap.get("email"));
+
+       //check if user exists by email
+       User user;
+       if (optionalUser.isPresent()){
+           user = optionalUser.get();
+       }else {
+           responseList.add("Incorrect username or password");
+           return new ResponseEntity<>(responseList, HttpStatus.NOT_FOUND);
+       }
+
+       Optional<Account> optionalAccount = accountRepository.findAccountByUserId(user.getUserId());
+
+       Account account;
+       if (optionalAccount.isPresent()) {
+           account = optionalAccount.get();
+       }else {
+           responseList.add("Could not find account");
+           return new ResponseEntity<>(responseList, HttpStatus.NOT_FOUND);
+       }
+
+       //check if password matches
+       if (account.getPassword().getPassword().equals(jsonMap.get("password"))) {
+           responseList.add(user);
+           return new ResponseEntity<>(responseList, HttpStatus.FOUND);
+        }
+
+        responseList.add("Incorrect username or password");
+       return new ResponseEntity<>(responseList, HttpStatus.NOT_FOUND);
+
+
+
+    }
 }
--- application/poolapp/src/main/java/com/fantasticfour/poolapp/repository/UserRepository.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/repository/UserRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
         //Searches for a users by name matching regex supplied.
         @Query(value = "SELECT * FROM user WHERE name REGEXP ?1", nativeQuery = true)
         List<User> findByNameMatchesRegex(String regex);
+
+        //search for a user by email. should be unique
+        Optional<User> findByEmail (String email);
 
 }
