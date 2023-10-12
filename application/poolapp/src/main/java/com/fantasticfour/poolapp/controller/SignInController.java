ab5722c08f5464893473b1c3ce60060d76021ae4
nexusstar12 <Nexusstar12@gmail.com>
2023-10-11 19:12:47 -0700

    Changed GetMapping to PostMapping

--- application/poolapp/src/main/java/com/fantasticfour/poolapp/controller/SignInController.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/controller/SignInController.java
@@ -12,10 +12,7 @@ import com.fantasticfour.poolapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
 import java.util.ArrayList;
@@ -35,7 +32,7 @@ public class SignInController {
     @Autowired
     private AccountRepository accountRepository;
 
-    @GetMapping("/")
+    @PostMapping("/")
     public ResponseEntity<List<Object>> signInUser (@RequestBody Map<String, String> jsonMap) {
 
         jsonMap.forEach((key, value) -> System.out.println("Key: " + key + ", Value: " + value));
