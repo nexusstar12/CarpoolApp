69b59e5b9fecad1e53360fb6d41323079fe4652f
nexusstar12 <Nexusstar12@gmail.com>
2023-10-23 11:42:56 -0700

    Implemented Profile and ProfileRepository class.

---
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/domain/Profile.java
@@ -0,0 +1,44 @@
+package com.fantasticfour.poolapp.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "profile")
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int profileId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    // Here, you might want to use an enum or another mechanism to distinguish between passenger and driver.
+    // For simplicity, let's use a String. Depending on your actual data model, this might change.
+    private String userType;
+
+    public int getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(int profileId) {
+        this.profileId = profileId;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
