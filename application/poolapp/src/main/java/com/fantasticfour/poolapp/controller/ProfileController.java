62de635341cc5d74158f741bbb146ef60a23affb
nasalito9 <45346008+nasalito9@users.noreply.github.com>
2023-10-24 14:56:06 -0700

    Profile Endpoint work in progress

---
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/controller/ProfileController.java
@@ -0,0 +1,36 @@
+package com.fantasticfour.poolapp.controller;
+
+import com.fantasticfour.poolapp.domain.Crew;
+import com.fantasticfour.poolapp.domain.Pool;
+import com.fantasticfour.poolapp.domain.Profile;
+import com.fantasticfour.poolapp.services.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/profile")
+public class ProfileController {
+
+    @Autowired
+    private ProfileService profileService;
+
+    @PostMapping("/")
+    public ResponseEntity<Profile> addProfile(@RequestBody Profile profile) {
+        Profile newProfile = profileService.addProfile(profile);
+        return new ResponseEntity<>(newProfile, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Profile> getProfilesFromPool(@RequestBody int poolInt){
+        List<Profile> profile = new ArrayList<>();
+        if(pool.getPoolList()){
+
+        }
+    }
+
+}
