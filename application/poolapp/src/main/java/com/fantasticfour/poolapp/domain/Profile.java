a9e16bb740c31afdbca0d933bf478152ffc75191
nexusstar12 <Nexusstar12@gmail.com>
2023-10-25 21:40:49 -0700

    Implemented getter and setter for crew in Profile and Pool class

--- application/poolapp/src/main/java/com/fantasticfour/poolapp/domain/Profile.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/domain/Profile.java
@@ -18,13 +18,13 @@ public class Profile {
     @JoinColumn(name = "pool_id", referencedColumnName = "poolId")
     private Pool pool;
 
-    /*@ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "crew_id", referencedColumnName = "crew_id")
-    private Crew crew;*/
+    private Crew crew;
 
+    private String userType;
     // Here, you might want to use an enum or another mechanism to distinguish between passenger and driver.
     // For simplicity, let's use a String. Depending on your actual data model, this might change.
-    private String userType;
 
     public int getProfileId() {
         return profileId;
@@ -42,6 +42,13 @@ public class Profile {
         this.userId = userId;
     }
 
+    public Crew getCrew() {
+        return crew;
+    }
+
+    public void setCrew(Crew crew) {
+        this.crew = crew;
+    }
     public String getUserType() {
         return userType;
     }
