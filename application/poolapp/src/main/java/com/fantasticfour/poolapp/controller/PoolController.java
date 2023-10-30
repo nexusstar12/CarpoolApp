9e88ebc9f88a2d64ebbf042d41e8ae6ccf4d6a0c
kkrstchn <kristiangoranov@yahoo.com>
2023-10-30 00:35:03 -0700

    created service for creating custom pool response. now returning mypools, available pools, and past pools

--- application/poolapp/src/main/java/com/fantasticfour/poolapp/controller/PoolController.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/controller/PoolController.java
@@ -38,6 +38,9 @@ public class PoolController {
     @Autowired
     private PassengerRepository passengerRepository;
 
+    @Autowired
+    private CustomPoolResponse customPoolResponse;
+
     @PostMapping("/join/{poolId}")
     public ResponseEntity<?> joinPool(@PathVariable int poolId, @RequestBody int profileId){
         poolService.addProfileToPool(poolId, profileId);
@@ -94,75 +97,27 @@ public class PoolController {
                                   .filter(pool -> pool.getStartTime().isAfter(currentTime))
                                   .toList();
 
-//        poolsByIdResponse.setMyPools(customPoolResponse.buildPoolResponseList(pools));
-        //create custom response for my pools
-        for (Pool pool : pools) {
-            PoolResponse poolResponse = new PoolResponse();
-
-            poolResponse.setPoolId(pool.getPoolId());
-            poolResponse.setStartLocation(pool.getStartLocation());
-            poolResponse.setEndLocation(pool.getEndLocation());
-            poolResponse.setStartTime(pool.getStartTime());
-            poolResponse.setDescription(pool.getDescription());
-
-            //get the driver from profile -> userid -> driver
-            CustomDriver customDriver = new CustomDriver();
-
-            int creatorProfileId = pool.getCreator().getProfileId();
-            //add driver to response
-            profileRepository.findProfileByProfileId(creatorProfileId)
-                    .ifPresent(driverProfile -> {
-
-                        User userDriver = driverProfile.getUserId();
-
-                        driverRepository.findByUser_UserId(userDriver.getUserId())
-                                .ifPresent(driver -> {
-                                    customDriver.setDriverId(driver.getDriverId());
-                                    customDriver.setName(userDriver.getName());
-                                    poolResponse.setDriver(customDriver);
-                                });
-
-                    } );
-
-            //get passengers
-
-            List<Profile> memberList = new ArrayList<>();
-             memberList.add(pool.getMember1());
-            memberList.add(pool.getMember2());
-            memberList.add(pool.getMember3());
-
-            for (Profile member: memberList) {
-                CustomPassenger customPassenger = new CustomPassenger();
-                if (member != null) {
-                      User userPassenger = member.getUserId();
-                      passengerRepository.findPassengerByUserId(userPassenger.getUserId())
-                              .ifPresent(passenger -> {
-                                  customPassenger.setPassengerId(passenger.getPassengerId());
-                                  customPassenger.setName(userPassenger.getName());
-                                  poolResponse.addPassenger(customPassenger);
-                              });
-                }
-            }
-
-            poolsByIdResponse.addMyPoolsIndex(poolResponse);
-
-            //for testing make custom class.
-            poolsByIdResponse.addAvailablePoolsIndex(poolResponse);
-            poolsByIdResponse.addPastPoolsIndex(poolResponse);
-
-        }//End For Each
+        //build custom response
+        poolsByIdResponse.setMyPools(customPoolResponse.buildPoolResponseList(myPools));
 
         //available pools -  private pools they are a member of
         List<Pool> availablePools = poolRepository.findByProfileId(profileId).stream()
                 .filter(Optional::isPresent)
                 .map(Optional::get)
+                .filter(pool -> pool.getStartTime().isAfter(currentTime))
                 .filter(Pool::isPublicOrPrivate)
                 .toList();
+
+        //add to custom response
+        poolsByIdResponse.setAvailablePools(customPoolResponse.buildPoolResponseList(availablePools));
 
         //past pools
         List<Pool> pastPools = pools.stream()
                 .filter(pool -> pool.getStartTime().isBefore(currentTime))
                 .toList();
+
+        //add to custom response
+        poolsByIdResponse.setPastPools(customPoolResponse.buildPoolResponseList(pastPools));
 
 
        return new ResponseEntity<>(poolsByIdResponse, HttpStatus.OK);
