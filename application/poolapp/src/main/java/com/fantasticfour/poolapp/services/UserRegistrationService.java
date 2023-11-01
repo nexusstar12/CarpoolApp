2b4ab9d2c1abc088f406560e53ad5779eabac412
nexusstar12 <Nexusstar12@gmail.com>
2023-10-31 18:27:33 -0700

    Added exception and error handling logging to better keep track of errors.

--- application/poolapp/src/main/java/com/fantasticfour/poolapp/services/UserRegistrationService.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/services/UserRegistrationService.java
@@ -6,11 +6,15 @@ import com.fantasticfour.poolapp.domain.Profile;
 import com.fantasticfour.poolapp.domain.User;
 import com.fantasticfour.poolapp.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserRegistrationService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserRegistrationService.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -26,31 +30,42 @@ public class UserRegistrationService {
 
     @Transactional
     public User registerUser(User user, boolean isDriver) {
-        // Save the user to the database
-        User savedUser = userRepository.save(user);
+        try {
+            log.info("Registering user with email: {}", user.getEmail());
 
-        // Create and save the passenger profile
-        Passenger passenger = new Passenger();
-        passenger.setUser(savedUser);
-        passengerService.addPassenger(passenger);
+            // Save the user to the database
+            User savedUser = userRepository.save(user);
+            log.info("User saved with ID: {}", savedUser.getUserId());
 
-        // If the user is also a driver, create and save the driver profile
-        if (isDriver) {
-            Driver driver = new Driver();
-            driver.setUser(savedUser);
-            // Driver-specific properties here
-            driverService.addDriver(driver);
+            // Create and save the passenger profile
+            Passenger passenger = new Passenger();
+            passenger.setUser(savedUser);
+            passengerService.addPassenger(passenger);
+            log.info("Passenger profile added for user ID: {}", savedUser.getUserId());
+
+            // If the user is also a driver, create and save the driver profile
+            if (isDriver) {
+                Driver driver = new Driver();
+                driver.setUser(savedUser);
+                // Driver-specific properties here
+                driverService.addDriver(driver);
+                log.info("Driver profile added for user ID: {}", savedUser.getUserId());
+            }
+
+            // Create a unified profile entry linking to the User's ID
+            Profile profile = new Profile();
+            profile.setUserId(savedUser);
+            profile.setUserType(isDriver ? "driver" : "passenger");
+            profileService.addProfile(profile);
+            log.info("Unified profile added for user ID: {}", savedUser.getUserId());
+
+            log.info("User registration completed for email: {}", user.getEmail());
+
+            return savedUser;
+
+        } catch (Exception e) {
+            log.error("Error during user registration for email: {}", user.getEmail(), e);
+            throw e;  // re-throwing to ensure the transactional annotation will rollback the transaction
         }
-
-        // Create a unified profile entry linking to the User's ID
-        Profile profile = new Profile();
-        profile.setUserId(savedUser);
-        profile.setUserType(isDriver ? "driver" : "passenger"); // Adjust this logic as needed
-        // Any logic that needs to connect specific profile entries with driver/passenger IDs here
-
-        profileService.addProfile(profile);
-
-        return savedUser;
     }
 }
-
