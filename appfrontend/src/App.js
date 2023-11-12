cbf87eca849053292c8544134e933ea69d397e3c
IamGemek <91393106+IamGemek@users.noreply.github.com>
2023-11-12 15:12:17 -0800

    New Outage Page

--- appfrontend/src/App.js
+++ appfrontend/src/App.js
@@ -8,6 +8,7 @@ import DriverProfilePage from "./pages/DriverProfilePage";
 import ListPoolPage from "./pages/ListPoolPage";
 import ListCrewPage from "./pages/ListCrewPage";
 import TermsAndConditions from "./pages/TermsAndConditions";
+import OutagePage from "./pages/OutagePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Bar from "./components/Bar";
 
@@ -35,6 +36,7 @@ function App() {
             <Route path="/driver-profile" element={<DriverProfilePage />} />
             <Route path="/my-pools" element={<ListPoolPage />} />
             <Route path="/my-crews" element={<ListCrewPage />} />
+            <Route path="/down" element={<OutagePage />} />
           </Routes>
         </Router>
       </div>
