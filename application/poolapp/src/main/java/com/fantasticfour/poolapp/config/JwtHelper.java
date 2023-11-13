a9121095b3e287b1de0152b94beee2dce7f2f288
kkrstchn <kristiangoranov@yahoo.com>
2023-11-12 23:45:30 -0800

    extended jwt expiration to 5 days, and try and fix cors issue

--- application/poolapp/src/main/java/com/fantasticfour/poolapp/config/JwtHelper.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/config/JwtHelper.java
@@ -17,7 +17,7 @@ import java.util.function.Function;
 
 @Component
 public class JwtHelper {
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60 * 24; //TOKEN VALID FOR  5HRS.
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60 * 24; //TOKEN VALID FOR  5 days.
 
     public String getUserNameFromJwtToken(String token) {
         return getClaimFromJwtToken(token,Claims::getSubject);
--- application/poolapp/src/main/java/com/fantasticfour/poolapp/config/SecurityConfig.java
+++ application/poolapp/src/main/java/com/fantasticfour/poolapp/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
+        http.cors().and()
+                .csrf(csrf -> csrf.disable())
                         .authorizeHttpRequests(auth -> auth.requestMatchers("/user/**").authenticated()
                         .requestMatchers("/api/signin/","/api/signin", "/api/searchbar**", "/api/signup**").permitAll()
                         .anyRequest().authenticated())
