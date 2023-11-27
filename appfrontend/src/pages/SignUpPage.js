50c63debc64e39e583ef4762fa1d664096abbb86
IamGemek <91393106+IamGemek@users.noreply.github.com>
2023-11-27 12:59:30 -0800

    Almost done, just need to handle bugs with  name

--- appfrontend/src/pages/SignUpPage.js
+++ appfrontend/src/pages/SignUpPage.js
@@ -124,38 +124,43 @@ export default function SignUp() {
     } else {
       setFirstNameError(null);
     }
-  
+
     if (!lastName) {
       setLastNameError("Required");
     } else {
       setLastNameError(null);
     }
-  
+
     if (!phoneNumber) {
       setPhoneError("Required");
     } else {
       setPhoneError(null);
     }
-  
+
     if (!email) {
       setEmailError("Required");
     } else {
       setEmailError(null);
     }
-  
+
     if (!password || password.trim() === "") {
       setPasswordError("Required");
     } else {
       setPasswordError(null);
     }
-    if (!firstNameError && !lastNameError && !phoneError && !emailError && !passwordError) {
+    if (
+      !firstNameError &&
+      !lastNameError &&
+      !phoneError &&
+      !emailError &&
+      !passwordError
+    ) {
       try {
       } catch (error) {
         setError(error.response.data.message);
       }
     }
   };
-
 
   const SuccessModal = () => {
     const handleRedirect = () => {
@@ -255,6 +260,36 @@ export default function SignUp() {
                     id="firstName"
                     label="First Name"
                     autoFocus
+                    onBlur={(e) => {
+                      const value = e.target.value;
+
+                      // Check for required field
+                      if (!value || value.trim() === "") {
+                        setFirstNameError("Required");
+                        setFirstNameCharError(null);
+                      } else {
+                        // Check for length
+                        if (value.length > 50) {
+                          setFirstNameError(
+                            "Enter a name less than 51 characters long."
+                          );
+                          setFirstNameCharError(null);
+                        } else {
+                          // Check for valid characters
+                          const firstNameRegex = /^[A-Za-z.-]+$/;
+                          if (!firstNameRegex.test(value)) {
+                            setFirstNameCharError(
+                              "Enter a valid first name consisting only of letters, hyphens, or periods."
+                            );
+                            setFirstNameError(null);
+                          } else {
+                            // Clear errors if everything is valid
+                            setFirstNameError(null);
+                            setFirstNameCharError(null);
+                          }
+                        }
+                      }
+                    }}
                     error={!!firstNameCharError || !!firstNameError}
                     helperText={firstNameCharError || firstNameError || ""}
                   />
@@ -267,6 +302,32 @@ export default function SignUp() {
                     label="Last Name"
                     name="lastName"
                     autoComplete="family-name"
+                    onBlur={(e) => {
+                      const value = e.target.value;
+
+                      if (!value || value.trim() === "") {
+                        setLastNameError("Required");
+                        setLastNameCharError(null);
+                      } else {
+                        if (value.length > 50) {
+                          setLastNameError(
+                            "Enter a name less than 51 characters long."
+                          );
+                          setLastNameCharError(null);
+                        } else {
+                          const lastNameRegex = /^[A-Za-z.-]+$/;
+                          if (!lastNameRegex.test(value)) {
+                            setLastNameCharError(
+                              "Enter a valid last name consisting only of letters, hyphens, or periods."
+                            );
+                            setLastNameError(null);
+                          } else {
+                            setLastNameError(null);
+                            setLastNameCharError(null);
+                          }
+                        }
+                      }
+                    }}
                     error={!!lastNameCharError || !!lastNameError}
                     helperText={lastNameCharError || lastNameError || ""}
                   />
@@ -281,6 +342,17 @@ export default function SignUp() {
                     autoComplete="phoneNumber"
                     error={!!phoneError}
                     helperText={phoneError}
+                    onBlur={(e) => {
+                      const value = e.target.value;
+
+                      if (!value || value.trim() === "") {
+                        setPhoneError("Required");
+                      } else if (!isPhoneNumberValid(value)) {
+                        setPhoneError("Invalid phone number");
+                      } else {
+                        setPhoneError(null);
+                      }
+                    }}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -293,6 +365,12 @@ export default function SignUp() {
                     autoComplete="email"
                     error={!!emailError}
                     helperText={emailError}
+                    onBlur={(e) => {
+                      const value = e.target.value;
+                      setEmailError(value.trim() === "" ? "Required" : null);
+                    }}
+                    error={!!emailError}
+                    helperText={emailError || ""}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -308,15 +386,11 @@ export default function SignUp() {
                     helperText={passwordError}
                     onBlur={(e) => {
                       const value = e.target.value;
-                  
-                      if (!value || value.trim() === "") {
-                        setPasswordError("Required");
-                      } else if (value.length < 8) {
-                        setPasswordError("Password must be at least 8 characters long");
-                      } else {
-                        setPasswordError(null);
-                      }
+
+                      setPasswordError(value.trim() === "" ? "Required" : null);
                     }}
+                    error={!!passwordError}
+                    helperText={passwordError || ""}
                   />
                 </Grid>
                 <Grid item xs={12}>
